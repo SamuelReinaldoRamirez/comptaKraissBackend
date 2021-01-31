b4481b3cf53abbeb64166726fb70ebaeb5e75971
SamuelReinaldoRamirez <samuel.reinaldo.ramirez@gmail.com>
2021-01-31 13:52:36 +0100

    back tout à fait ok meme pour le dotenv je crois bien

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
+require("dotenv").config();
+const webpack = require('webpack');
 const express = require('express')
 const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express()
 const port = 5000
+var cors = require('cors')
+
+var ngrokFront = process.env.REACT_APP_ngrokFront
+
+/*
+module.exports = {
+    webpack: (config) => {
+      const env = {  };
+      config.plugins.push(new webpack.DefinePlugin(env));
+      return config;
+    },
+  }*/
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://"+ngrokFront+".ngrok.io/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.options('/handle', cors())
+
 
 var mysql = require('mysql');
+console.log("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+console.log(process.env.dbpassword);
 
   var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
+    password: process.env.dbpassword,
     database: "compta_kraiss"
   });
 
@@ -24,7 +50,7 @@ var mysql = require('mysql');
   });
 
 
-router.put('/handle',(req, res) => {
+router.put('/handle', cors(), (req, res, next) => {
     //code to perform particular action.
     //To access POST variable use req.body()methods.
     console.log(req.body);
@@ -35,7 +61,7 @@ router.put('/handle',(req, res) => {
         console.log(result);
     });*/
 
-      var sql = "UPDATE orders SET order_state = 6 WHERE order_id=1";
+      var sql = "UPDATE orders SET order_state = 2 WHERE order_id=1";
       con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result.affectedRows + " record(s) updated");
